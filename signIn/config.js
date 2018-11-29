cf37165480d5fa71ac5d3c4aea7ac23b2926605a
Bob Main <bobmain49@gmail.com>
2018-11-29 17:48:56 -0600

    started signin userflow

---
+++ signIn/config.js
@@ -0,0 +1,8 @@
+let config = {
+  apiKey: "AIzaSyC0ISNtKDs1pV284bjnrOSVmtzQd7tvciI",
+  authDomain: "bus-derby-multi.firebaseapp.com",
+  databaseURL: "https://bus-derby-multi.firebaseio.com",
+  projectId: "bus-derby-multi",
+  storageBucket: "bus-derby-multi.appspot.com",
+  messagingSenderId: "420547108228"
+}
