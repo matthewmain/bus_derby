ee558985d72cb3b6697a7ebc242bb6f7bdb441bb
Bob Main <bobmain49@gmail.com>
2018-12-21 15:51:34 -0600

    adding keySwap obj

--- multi/multiplayer_key_swap.js
+++ multi/multiplayer_key_swap.js
@@ -1,8 +1,16 @@
 function multiplayer_key_swap (key, dir) {
-  
-  [ hash, name ] = document.location.hash.split(/[ #=]+/).filter(Boolean)
 
+  [ hash, name ] = document.location.hash.split(/[ #=]+/).filter(Boolean)
+  console.log('keycode,',key)
   if (["p1","p2"].includes(hash))  {
+    const keySwap = {
+      "p2": {
+        80: 38,
+        222: 39,
+        168: 40,
+        76: 37,
+      }
+    }
     if (hash === 'p1') {
       console.log('player1', hash, key, dir)
     } else if(hash === 'p2') {
